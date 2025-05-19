7a24b359ebfa84899004131f546129e3c41f17f9
Raksha Tiwari <91082094+RakshaTiwari921@users.noreply.github.com>
2025-05-19 21:39:32 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Dhoni from "./Components/Images/Dhoni.jpg"
+import Virat from "./Components/Images/virat.jpg"
+import Rohit from "./Components/Images/Rohit.jpg" 
+import Suresh from "./Components/Images/Suresh.jpg"
+import KLRahul from "./Components/Images/KLRahul.jpg"
+import "./Components/style.css"
+
+
+function App()
+{
+
+    return (
+        <div>
+           <img src = {Dhoni} alt="" />  
+           <img src = {Rohit} alt="" /> 
+           <img src = {Virat} alt="" />  
+           <img src = {Suresh} alt="" />  
+           <img src = {KLRahul} alt="" />  
+         </div>
+         
+    )
+}
+
+export default App;
